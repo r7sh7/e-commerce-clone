0ac54797bdda57fd95e6c4e5829250c51afef9f3
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2022-01-31 22:43:49 +0530

    added mui theme

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import {
   AppBar,
   Badge,
   Container,
+  CssBaseline,
   Link,
   Toolbar,
   Typography,
@@ -14,8 +15,35 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useSelector } from "react-redux";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title }) => {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
   const { data: session, status } = useSession();
 
@@ -23,46 +51,47 @@ const Layout = ({ children }) => {
   return (
     <div>
       <Head>
-        <title>E-commerce-clone</title>
+        <title>{title ? `${title} - Amazon Clone` : "Amazon Clone"}</title>
       </Head>
-      <AppBar position="sticky" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link style={{ textDecoration: "none" }}>
-              <Typography className={classes.brand}>Amazon</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            {status === "authenticated" ? (
-              <Typography className={classes.headerIcons}>
-                Hello, {session.user.name}
-              </Typography>
-            ) : (
-              <Link onClick={signIn} className={classes.headerIcons}>
-                <Typography>Hello, Sign In</Typography>
-              </Link>
-            )}
-            <NextLink href="/checkout" passHref>
-              <Link className={classes.headerIcons}>
-                <Badge color="warning" badgeContent={itemsCount}>
-                  <ShoppingCartIcon />
-                </Badge>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="sticky" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link style={{ textDecoration: "none" }}>
+                <Typography className={classes.brand}>Amazon</Typography>
               </Link>
             </NextLink>
-            {
-              status === "authenticated" && (
-                // {/* <NextLink href="/login" passHref> */}
-                <Link className={classes.headerIcons} onClick={signOut}>
-                  <LogoutIcon />
+            <div className={classes.grow}></div>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              {status === "authenticated" ? (
+                <Typography className={classes.headerIcons}>
+                  Hello, {session.user.name}
+                </Typography>
+              ) : (
+                <Link onClick={signIn} className={classes.headerIcons}>
+                  <Typography>Hello, Sign In</Typography>
                 </Link>
-              )
-              // {/* </NextLink> */}
-            }
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
+              )}
+              <NextLink href="/cart" passHref>
+                <Link className={classes.headerIcons}>
+                  <Badge color="primary" badgeContent={itemsCount}>
+                    <ShoppingCartIcon />
+                  </Badge>
+                </Link>
+              </NextLink>
+              {status === "authenticated" && (
+                <NextLink href="/" passHref>
+                  <Link className={classes.headerIcons} onClick={signOut}>
+                    <LogoutIcon />
+                  </Link>
+                </NextLink>
+              )}
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+      </ThemeProvider>
     </div>
   );
 };
