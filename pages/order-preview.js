745075ba567b45d259b37e5d79276e20cd9b50c1
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2022-02-02 00:01:34 +0530

    order preview screen done

---
+++ pages/order-preview.js
@@ -0,0 +1,202 @@
+import {
+  Button,
+  Card,
+  Grid,
+  List,
+  ListItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import Layout from "../components/Layout";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import CheckoutWizard from "../components/CheckoutWizard";
+import { addDoc, doc, serverTimestamp, collection } from "firebase/firestore";
+import { db } from "../firebase";
+import { useSession } from "next-auth/react";
+
+const OrderPreview = () => {
+  const { cartItems, shippingAddress, paymentMethod } = useSelector(
+    (state) => state.cart.cart
+  );
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+  const itemsPrice = round2(
+    cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+  );
+  const shippingPrice = itemsPrice > 200 ? 0 : 15;
+  const taxPrice = round2(itemsPrice * 0.15);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+
+  async function handleOrder() {
+    const docRef = doc(db, "users", session.user.email);
+    const colRef = collection(docRef, "orders");
+    const newDocRef = await addDoc(colRef, {
+      cartItems,
+      shippingAddress,
+      paymentMethod,
+      timestamp: serverTimestamp(),
+    });
+    localStorage.removeItem("cartItems");
+    console.log(newDocRef);
+    router.push("/order-complete");
+  }
+  return (
+    <Layout>
+      <CheckoutWizard activeStep={3} />
+      <Typography variant="h1" component="h1">
+        Place Order
+      </Typography>
+      <Grid container spacing={1}>
+        <Grid item md={9} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Shipping Address
+                </Typography>
+              </ListItem>
+              <ListItem>
+                {shippingAddress.fullName}, {shippingAddress.address},{" "}
+                {shippingAddress.city}, {shippingAddress.postalCode},{" "}
+                {shippingAddress.country}
+              </ListItem>
+            </List>
+          </Card>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Payment Method
+                </Typography>
+              </ListItem>
+              <ListItem>{paymentMethod}</ListItem>
+            </List>
+          </Card>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography component="h2" variant="h2">
+                  Order Items
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Image</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {cartItems.map((item) => (
+                        <TableRow key={item.id}>
+                          <TableCell>
+                            <Image
+                              src={item.image}
+                              alt={item.title}
+                              width={50}
+                              height={50}
+                            ></Image>
+                          </TableCell>
+                          <TableCell>
+                            <Typography color="primary">
+                              {item.title}
+                            </Typography>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>{item.quantity}</Typography>
+                          </TableCell>
+                          <TableCell align="right">
+                            <Typography>${item.price}</Typography>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Typography variant="h2">Order Summary</Typography>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Items: </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${itemsPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Tax: </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${taxPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Shipping: </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${shippingPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>
+                      <strong>Total: </strong>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">
+                      <strong>${totalPrice}</strong>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={handleOrder}
+                >
+                  Place Order
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default OrderPreview;
