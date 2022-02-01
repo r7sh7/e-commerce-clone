9d239cb1b39ffa4b672ac3e64c16cb15e7950737
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2022-02-02 00:01:42 +0530

    order complete screen done

---
+++ pages/order-complete.js
@@ -0,0 +1,26 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import React from "react";
+import Layout from "../components/Layout";
+
+const OrderComplete = () => {
+  const router = useRouter();
+  return (
+    <Layout>
+      <Box textAlign="center">
+        <Typography component="h1" variant="h1" textAlign="center">
+          Your Order has been placed successfully!
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => router.push("/")}
+        >
+          Continue Shopping
+        </Button>
+      </Box>
+    </Layout>
+  );
+};
+
+export default OrderComplete;
